6419ad36b5bc5aa0f4f33b52a6134458656649a7
Michael Caverly <m.caverly30@gmail.com>
2020-03-12 15:36:31 +0000

    data model update

--- GEKKO/Bioreactor/bioreactor.py
+++ GEKKO/Bioreactor/bioreactor.py
@@ -6,10 +6,9 @@ Cultivation of isolated plant cells in suspension culture (adapted from APMonito
 Munack A., Posten C. (1989): Design of optimal dynamical experiments for parameter estimation, Proceedings of the Americal Control Conference, Vol. 4, 2010-2016. 
 '''
 
-'''
 # Data init
 data_file = np.loadtxt('GEKKO/Bioreactor/data.txt', delimiter=',')
 time = data_file[:,0]
-data = data_file[:,1]
-print(data[0:2])
-'''
+cells_data = data_file[:,1]
+substrate_data = data_file[:,2]
+print(substrate_data[0:10])
