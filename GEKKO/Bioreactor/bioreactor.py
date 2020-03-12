ffb92ff39df0853120c98affc1dc14da83a03074
Michael Caverly <m.caverly30@gmail.com>
2020-03-12 19:05:38 +0000

    plotting updates

--- GEKKO/Bioreactor/bioreactor.py
+++ GEKKO/Bioreactor/bioreactor.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 '''
 Cultivation of isolated plant cells in suspension culture (adapted from APMonitor example for Cell Cultrure). Only cell and substrate are considered (product is the cells in this process). A set of hypothetical experimental data is used to fit theoretical model to experiment.
 
@@ -8,7 +8,31 @@ Munack A., Posten C. (1989): Design of optimal dynamical experiments for paramet
 
 # Data init
 data_file = np.loadtxt('GEKKO/Bioreactor/data.txt', delimiter=',')
-time = data_file[:,0]
-cells_data = data_file[:,1]
-substrate_data = data_file[:,2]
-print(substrate_data[0:10])
+time = data_file[:, 0]
+cells_data = data_file[:, 1]
+substrate_data = data_file[:, 2]
+#print(substrate_data[0:10])
+
+# plot curve(s)
+fig, ax1 = plt.subplots()
+
+color = 'tab:red'
+marker = 'x'
+ax1.set_xlabel('Time (h)')
+ax1.set_ylabel('Biomass (g/L)', color=color)
+ax1.plot(time, cells_data, color=color, marker=marker, linestyle='None')
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color = 'tab:blue'
+marker = '.'
+ax2.set_ylabel(
+    'Substrate (g/L)', color=color)  # we already handled the x-label with ax1
+ax2.plot(time, substrate_data, color=color, marker=marker, linestyle='None')
+ax2.tick_params(axis='y', labelcolor=color)
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+
+# save output (required for Repl.it)
+plt.savefig('GEKKO/Bioreactor/model_plot.png')
