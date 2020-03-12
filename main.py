af8d557be4d11e5cb1573e292774829a17ad4729
Michael Caverly <m.caverly30@gmail.com>
2020-03-12 13:12:32 +0000

    Bioreactor model init. Refactor main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-from GEKKO import mixed_int_nonlinear
+import os
+
+for dirpath, dirnames, files in os.walk('.'):
+    #print(f'Found directory: {dirpath}')
+    for file_name in files:
+        if file_name == 'bioreactor.py':
+          print("Running " + file_name) 
+          exec(open(dirpath + '/' + file_name).read())
