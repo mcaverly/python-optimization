8ab7787ac43419eac055849f3097b33ad97a0eee
Michael Caverly <m.caverly30@gmail.com>
2020-03-19 14:52:13 +0000

    O2 and CO2 models added

--- GEKKO/Bioreactor/bioreactor.py
+++ GEKKO/Bioreactor/bioreactor.py
@@ -13,30 +13,47 @@ data_file = np.loadtxt('GEKKO/Bioreactor/data.txt', delimiter=',')
 time = data_file[:, 0]
 cells_data = data_file[:, 1]
 substrate_data = data_file[:, 2]
+o2_data = data_file[:, 3]
+co2_data = data_file[:, 4]
 
 # GEKKO model init
 m = GEKKO(remote=False)
-m.time = time  #np.linspace(0,20,nt)
+m.time = time
 
 # parameters
-rsmax = m.FV(value=0.05, lb=0); rsmax.STATUS = 1
-ks = m.FV(value=0.2, lb=0); ks.STATUS = 1
-yxs = m.FV(value=5.6, lb=0); ks.STATUS = 1
-mum = m.FV(value=0.118, lb=0); ks.STATUS = 1
+rf = m.FV(value=0.5, lb=0, name='R_F (?)'); rf.STATUS = 1
+rsmax = m.FV(value=0.01519, lb=0, name='rs_max (%/g-d)'); rsmax.STATUS = 1
+ks = m.FV(value=0.186, lb=0); ks.STATUS = 1
+yxs = m.FV(value=5.60, lb=0); yxs.STATUS = 1
+mum = m.FV(value=0.118, lb=0, name='mu_maintenance (1/d)'); mum.STATUS = 1
+romax = m.FV(value=85, lb=0, name="ro_max (%/g-d)"); romax.STATUS = 1
+ko = m.FV(value= 6.32, lb=0, name="k_o (%)"); ko.STATUS = 1
+osat = m.FV(value= 86.2, lb=0, name="o_sat (%)"); osat.STATUS = 1
+kL = m.FV(value= 0.0137, lb=0, name="k_L (h/L-d)"); kL.STATUS = 1
+yco = m.FV(value=0.897, lb=0); yxs.STATUS = 1
+vL = m.FV(value=0.05); vL.STATUS = 1
 
 # variables
 rs = m.CV(); rs.FSTATUS = 1
-mu = m.CV(); rs.FSTATUS = 1
-s = m.CV(value=substrate_data); s.FSTATUS = 1
-x = m.CV(value=cells_data); s.FSTATUS = 1
+mu = m.CV(); mu.FSTATUS = 1
+ro = m.CV(); ro.FSTATUS = 1
+rc = m.CV(); rc.FSTATUS = 1
+s = m.CV(value=substrate_data, lb=0); s.FSTATUS = 1
+x = m.CV(value=cells_data, lb=0); x.FSTATUS = 1
+o = m.CV(value=o2_data, lb=0); o.FSTATUS = 1
+c = m.CV(value=co2_data, lb=0); c.FSTATUS = 1
 
 # intermediate equations
-m.Equation(rs == rsmax * (s / (ks + s)))
+m.Equation(rs == rsmax * s / (ks + s))
 m.Equation(mu == yxs * rs - mum)
+m.Equation(ro == romax * o / (osat + o))
+m.Equation(rc == yco * ro)
 
 # dynamic equations
-m.Equation(s.dt() == -rs * x)
+m.Equation(s.dt() == -rs * x + rf)
 m.Equation(x.dt() == mu * x)
+m.Equation(ro * x == (osat - o) * kL * vL)
+m.Equation(c == rc * x / vL)
 
 # regression mode
 m.options.IMODE = 5  # dynamic estimation
@@ -44,11 +61,12 @@ m.options.NODES = 5  # collocation nodes
 m.options.EV_TYPE = 2  # squared error
 m.solve(disp=True)  # display solver output
 
-# print parameters
-print('Optimized, rsmax = ' + str(rsmax.value[0]) 
-  + ' ks = ' + str(ks.value[0])
-  + ' yxs = ' + str(yxs.value[0])
-  + ' mum = ' + str(mum.value[0])
+# print regressed parameters
+print(
+  'rsmax = ' + str(rsmax.value[0]) + '\n'
+  + 'ks = ' + str(ks.value[0]) + '\n'
+  + 'yxs = ' + str(yxs.value[0]) + '\n'
+  + 'mum = ' + str(mum.value[0]) + '\n'
 )
 
 #
